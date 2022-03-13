49241bc687d7299b676b41cb9f39dcabe3ac93d6
Soumik <tutul.smk@gmail.com>
2022-03-13 20:18:54 +0530

    commit

---
+++ client/build/precache-manifest.989d55e33476bdd51dc41a21942a7ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77600e3ef50d2284fb560921a7614a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb2aca79222151c227c",
+    "url": "/static/js/2.b1263b3e.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/static/js/2.b1263b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0458c79119e7767d32",
+    "url": "/static/js/main.a09a5fbb.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
